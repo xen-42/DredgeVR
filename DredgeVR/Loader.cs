3a3068224a73c26bd1a7702644aaada4d583ace4
Nick <nicholasconnors@hotmail.ca>
2023-10-11 22:23:14 -0400

    Add dredge VR manifest (makes it appear in steam home)

--- DredgeVR/Loader.cs
+++ DredgeVR/Loader.cs
@@ -1,5 +1,8 @@
-﻿using HarmonyLib;
+﻿using DredgeVR.Helpers;
+using HarmonyLib;
 using System;
+using System.IO;
+using System.Reflection;
 using Unity.XR.OpenVR;
 using UnityEngine;
 using UnityEngine.XR.Management;
@@ -43,6 +46,19 @@ namespace DredgeVR
 			if (managerSettings.activeLoader == null) throw new Exception("Cannot initialize OpenVR Loader");
 
 			managerSettings.StartSubsystems();
+
+			SteamVR_Settings.instance.pauseGameWhenDashboardVisible = true;
+
+			// Makes it so that Dredge appears as a VR game in your Steam Home
+			var dredgeFolder = Directory.GetParent(Assembly.GetExecutingAssembly().Location).Parent.Parent.FullName;
+			var manifestPath = Path.Combine(dredgeFolder, @"DREDGE_Data\StreamingAssets\dredge.vrmanifest");
+			ApplicationManifestHelper.UpdateManifest(manifestPath,
+													"steam.app.1562430",
+													"https://steamcdn-a.akamaihd.net/steam/apps/1562430/header.jpg",
+													"Dredge VR",
+													"Dredge VR mod",
+													steamBuild: SteamManager.Initialized,
+													steamAppId: 1562430);
 		}
 	}
 }
