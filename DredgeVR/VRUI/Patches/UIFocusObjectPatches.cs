20d5ebc4a9889d62a8b100fa2bf0a0476c4410ae
Nick <nicholasconnors@hotmail.ca>
2023-10-16 17:22:10 -0400

    Fix UI focus (missing offset)

---
+++ DredgeVR/VRUI/Patches/UIFocusObjectPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace DredgeVR.VRUI.Patches;
+
+[HarmonyPatch(typeof(UIFocusObject))]
+public static class UIFocusObjectPatches
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(UIFocusObject.Update))]
+	public static void UIFocusObject_Update(UIFocusObject __instance)
+	{
+		if (__instance.hasTargetObject)
+		{
+			//var offset = __instance.targetObject.GetComponent<RectTransform>().rect.center;
+			__instance.transform.position = __instance.targetObject.transform.position;
+			__instance.transform.rotation = __instance.targetObject.transform.rotation;
+			__instance.container.transform.localPosition = Vector3.zero;
+			var scaleFactor = __instance.targetObject.transform.lossyScale.x / __instance.transform.lossyScale.x;
+			__instance.container.transform.localScale = Vector3.one * scaleFactor;
+		}
+	}
+}
