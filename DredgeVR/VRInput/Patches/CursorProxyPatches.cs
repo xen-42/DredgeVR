20fe7a191519a44b931bcff4e74e244ec4f8c890
Nick <nicholasconnors@hotmail.ca>
2023-10-17 15:06:59 -0400

    Bring over some grid fixes

---
+++ DredgeVR/VRInput/Patches/CursorProxyPatches.cs
@@ -0,0 +1,41 @@
+﻿using Cinemachine.Utility;
+using DredgeVR.VRCamera;
+using DredgeVR.VRInput;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DredgeVR.VRUI.Patches;
+
+[HarmonyPatch(typeof(CursorProxy))]
+public static class CursorProxyPatches
+{
+	private static Canvas _canvas;
+
+	/// <summary>
+	/// Cursor proxy position is used for navigating grid inventory menus
+	/// </summary>
+	/// <param name="__instance"></param>
+	/// <param name="__result"></param>
+	/// <returns></returns>
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(CursorProxy.Update))]
+	public static void CursorProxy_Update(CursorProxy __instance)
+	{
+		_canvas ??= __instance.GetComponentInParent<Canvas>();
+		var pixelHalfSize = new Vector3(_canvas.pixelRect.size.x, _canvas.pixelRect.size.y, 0f) / 2f;
+
+		if (VRHand.DominantHand?.LaserPointerEnd?.transform?.position is Vector3 laserPointerPos)
+		{
+			/*
+			var localPos = _canvas.transform.InverseTransformPoint(laserPointerPos.ProjectOntoPlane(_canvas.transform.forward));
+			__instance.cursorSquare.transform.position = __instance.transform.TransformPoint(localPos);
+			*/
+			__instance.cursorSquare.transform.position = laserPointerPos;
+			var localLaserPosition = __instance.cursorSquare.transform.localPosition;
+			__instance.cursorPos = localLaserPosition + pixelHalfSize;
+		}
+
+		var localPosition = __instance.cursorPos - pixelHalfSize;
+		__instance.cursorSquare.transform.position = __instance.transform.TransformPoint(localPosition);
+	}
+}
