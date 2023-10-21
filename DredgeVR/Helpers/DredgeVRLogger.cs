7a01bba7cf9808b4e8570c83d996f77e3f849c91
Nick <nicholasconnors@hotmail.ca>
2023-10-21 13:15:56 -0400

    I'm dumb

--- DredgeVR/Helpers/DredgeVRLogger.cs
+++ DredgeVR/Helpers/DredgeVRLogger.cs
@@ -4,12 +4,12 @@ namespace DredgeVR.Helpers;
 
 public static class DredgeVRLogger
 {
-	public static void Info(object msg) => DredgeVRLogger.Info(msg);
-	public static void Info(params object[] objs) => DredgeVRLogger.Info(string.Join(", ", objs));
-	public static void Debug(object msg) => DredgeVRLogger.Debug(msg);
-	public static void Debug(params object[] objs) => DredgeVRLogger.Debug(string.Join(", ", objs));
-	public static void Warn(object msg) => DredgeVRLogger.Warn(msg);
-	public static void Warn(params object[] objs) => DredgeVRLogger.Warn(string.Join(", ", objs));
-	public static void Error(object msg) => DredgeVRLogger.Error(msg);
-	public static void Error(params object[] objs) => DredgeVRLogger.Error(string.Join(", ", objs));
+	public static void Info(object msg) => WinchCore.Log.Info(msg);
+	public static void Info(params object[] objs) => WinchCore.Log.Info(string.Join(", ", objs));
+	public static void Debug(object msg) => WinchCore.Log.Debug(msg);
+	public static void Debug(params object[] objs) => WinchCore.Log.Debug(string.Join(", ", objs));
+	public static void Warn(object msg) => WinchCore.Log.Warn(msg);
+	public static void Warn(params object[] objs) => WinchCore.Log.Warn(string.Join(", ", objs));
+	public static void Error(object msg) => WinchCore.Log.Error(msg);
+	public static void Error(params object[] objs) => WinchCore.Log.Error(string.Join(", ", objs));
 }
