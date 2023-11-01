28bd14cf100d0de23f5bb7c1209600dc552984d9
Nick <nicholasconnors@hotmail.ca>
2023-10-31 20:37:46 -0400

    Set parent extension

---
+++ DredgeVR/Helpers/UnityExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DredgeVR.Helpers;
+
+public static class UnityExtensions
+{
+	public static GameObject SetParent(this GameObject go, Transform parent)
+	{
+		go.transform.parent = parent;
+		go.transform.localPosition = Vector3.zero;
+		go.transform.localRotation = Quaternion.identity;
+		return go;
+	}
+}
