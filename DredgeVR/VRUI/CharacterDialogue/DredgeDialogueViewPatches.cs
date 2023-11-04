d71b7641f79739a320017f5a7f957082e6a8bcd4
Nick <nicholasconnors@hotmail.ca>
2023-11-04 02:18:11 -0400

    Layer character portrait background

---
+++ DredgeVR/VRUI/CharacterDialogue/DredgeDialogueViewPatches.cs
@@ -0,0 +1,24 @@
+﻿using DredgeVR.Helpers;
+using HarmonyLib;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace DredgeVR.VRUI.CharacterDialogue;
+
+[HarmonyPatch(typeof(DredgeDialogueView))]
+public static class DredgeDialogueViewPatches
+{
+	[HarmonyPostfix]
+	[HarmonyPatch(nameof(DredgeDialogueView.ShowPortrait), new Type[] { typeof(SpeakerData) })]
+	public static void DredgeDialogueView_ShowPortrait(DredgeDialogueView __instance)
+	{
+		// Bit hacky
+		// Move the background back a bit if it exists
+		var background = __instance.characterPortraitContainer.GetComponentsInChildren<Transform>().FirstOrDefault(x => x.name.Contains("Background"));
+		if (background != null)
+		{
+			background.transform.localPosition += Vector3.forward * 50f;
+		}
+	}
+}
